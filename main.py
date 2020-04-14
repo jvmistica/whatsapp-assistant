99d7b707cd10a773263101316d479849fe4281a0
Joann Mistica <zombossjoey@tutanota.com>
2020-04-14 23:24:22 +0800

    Cleaned up code.

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ from cloud_sql import search_table, insert_record, show_record, update_record, d
 app = Flask(__name__)
 local_tz = pytz.timezone("Singapore")
 logging.basicConfig(level=logging.INFO)
-msg_samples = ["Gotcha.", "Done!", "Okay, I saved it.", "Consider it done.", "Saved it.", "All good.", "Got it."]
+msg_samples = ["Gotcha.",
+               "Done!",
+               "Okay, I saved it.",
+               "Consider it done.",
+               "Saved it.",
+               "All good.",
+               "Got it."]
 
 
 @app.route("/sms", methods=["POST"])
@@ -22,19 +28,19 @@ def sms_reply():
     logging.info("Received a message with action '%s' and text '%s'", action, text)
 
     if "what can i make?" in action:
-        possibilities = ", ".join(x_possible_recipes())
+        possibilities = ", ".join(search_for_recipes())
         resp.message(f"You can make the following: {possibilities}")
         return str(resp)
     elif action[:3] == "new":
-        x_new_record(text, action)
+        new_record(text, action)
     elif action[:4] == "show":
-        txt = x_show_record(text, action)
+        txt = view_record(text, action)
         resp.message(txt)
         return str(resp)
     elif action[:4] == "edit":
-        x_edit_record(text, action)
+        edit_record(text, action)
     elif action[:6] == "delete":
-        x_delete_record(text, action)
+        remove_record(text, action)
         resp.message(f"{action.split()[1].replace(':', '').capitalize()} '{text}' is deleted.")
         return str(resp)
     else:
@@ -44,10 +50,12 @@ def sms_reply():
     return str(resp)
 
 
-def x_new_record(text, action):
+def new_record(text, action):
     logging.info("Creating new record for '%s' and text '%s'", action, text)
     if "note" in action:
-        timestamp = datetime.now().replace(tzinfo=pytz.utc).astimezone(local_tz).strftime("%m/%d/%y %H:%M")
+        timestamp = datetime.now().replace(tzinfo=pytz.utc) \
+                                  .astimezone(local_tz) \
+                                  .strftime("%m/%d/%y %H:%M")
         title = timestamp
         insert_record("Notes", title=title, details=text.capitalize())
     elif "recipe" in action:
@@ -61,7 +69,7 @@ def x_new_record(text, action):
         pass
 
 
-def x_edit_record(text, action):
+def edit_record(text, action):
     logging.info("Editing '%s' record with text '%s'.", action, text)
     if "note" in action:
         title = text.split("->")[0].strip()
@@ -82,7 +90,7 @@ def x_edit_record(text, action):
         pass
 
 
-def x_show_record(text, action):
+def view_record(text, action):
     logging.info("Showing '%s' records.", action)
     table_name = action.replace("show ", "").replace(".", "").capitalize()
     if table_name == "Notes":
@@ -91,17 +99,17 @@ def x_show_record(text, action):
     elif table_name in ["Items", "Recipes"]:
         result = "\n".join([i[0] for i in search_table(table_name, "title")])
     else:
-        result = x_show_details(text, action)
+        result = show_details(text, action)
     return result
 
 
-def x_delete_record(text, action):
+def remove_record(text, action):
     logging.info("Deleting '%s' record '%s'.", action, text)
     table_name = action.replace("delete ", "").replace(":", "").capitalize() + "s"
     delete_record(table_name, text)
 
 
-def x_show_details(text, action):
+def show_details(text, action):
     logging.info("Showing details for '%s' record '%s'.", action, text)
     table_name = action.replace("show ", "").replace(".", "").capitalize()
     if table_name == "Notes":
@@ -116,23 +124,18 @@ def x_show_details(text, action):
     return result
 
 
-def x_possible_recipes():
+def search_for_recipes():
     logging.info("Looking for recipes.")
     items = [i[0] for i in search_table("Items", "title")]
     recipes = [i[0] for i in search_table("Recipes", "title")]
-    logging.info("Looking for recipes. Items: %s", items)
-    logging.info("Looking for recipes. Recipes: %s", recipes)
     matches = []
 
     for text in recipes:
         recipe_details = show_record("Recipes", title=text)
-        needed_items = [i.replace("and ", "").replace(".", "").capitalize() for i in recipe_details[2].split(", ")]
-        logging.info("Looking for recipe details. Recipe: %s, Details: %s", text, needed_items)
+        needed_items = [i.replace("and ", "").replace(".", "").capitalize() \
+                        for i in recipe_details[2].split(", ")]
         if not set(needed_items) - set(items):
             matches.append(text)
-            logging.info("Got a match!")
-        else:
-            logging.info("For %s, you're missing the following: %s", text, set(needed_items) - set(items))
 
     return matches
 
